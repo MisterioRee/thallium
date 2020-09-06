2471658a93e25fcddbb0f0eb0ccc9a15ebf16ed3
Muhammad Rehan <mrmuhammadrehan65@gmail.com>
2020-09-06 13:19:17 +0400

    Ng-UI

--- API/api/controller/userController.js
+++ API/api/controller/userController.js
@@ -1,7 +1,7 @@
 const User = require('../models/userModel');
 
 
-exports.signUp = ('/signup', (req, res, next) => {
+exports.signUp = (req, res, next) => {
 
     User.find({ email: req.body.email })
         .exec()
@@ -38,5 +38,68 @@ exports.signUp = ('/signup', (req, res, next) => {
                 });
             }
         })
-});
+}
 
+exports.login = (req, res, next) => {
+
+    User.find({ email: req.body.email })
+        .exec()
+        .then((user) => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: "Auth Faild"
+                });
+            } else {
+                bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                    if (err) {
+                        return res.status(401).json({
+                            message: 'Auth Faild'
+                        });
+                    }
+                    if (result) {
+                        const tocken = jwt.sign({
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                            'secrete',
+                            {
+                                expiresIn: "1h"
+                            },
+
+                        );
+                        return res.status(200).json({
+                            message: "Auth Successfull",
+                            tocken: tocken
+                        });
+                    }
+                    return res.status(401).json({
+                        message: "Auth Faild"
+                    });
+                })
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err
+            });
+        })
+}
+
+exports.deleteUser = (req, res, next) => {
+    const userId = req.body.userId;
+
+    User.deleteOne({ _id: userId })
+        .exec()
+        .then((result) => {
+            console.log(result);
+            res.status(200).json({
+                message: 'User Deleted Successfully'
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+}
--- API/api/routes/user.js
+++ API/api/routes/user.js
@@ -12,70 +12,10 @@ const saltRounds = 10;
 router.post('/signup', UserController.signUp);
 
 
-router.post('/login', (req, res, next) => {
-
-    User.find({ email: req.body.email })
-        .exec()
-        .then((user) => {
-            if (user.length < 1) {
-                return res.status(401).json({
-                    message: "Auth Faild"
-                });
-            } else {
-
-                bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                    if (err) {
-                        return res.status(401).json({
-                            message: 'Auth Faild'
-                        });
-                    }
-                    if (result) {
-                        const tocken = jwt.sign({
-                            email: user[0].email,
-                            userId: user[0]._id
-                        },
-                            'secrete',
-                            {
-                                expiresIn: "1h"
-                            },
-
-                        );
-                        return res.status(200).json({
-                            message: "Auth Successfull",
-                            tocken: tocken
-                        });
-                    }
-                    return res.status(401).json({
-                        message: "Auth Faild"
-                    });
-                })
-            }
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: err
-            });
-        })
-});
+router.post('/login', UserController.login)
 
 
-// router.delete('/:userId', (req, res, next) => {
-//     const userId = req.body.userId;
-
-//     User.deleteOne({ _id: userId })
-//         .exec()
-//         .then((result) => {
-//             console.log(result);
-//             res.status(200).json({
-//                 message: 'User Deleted Successfully'
-//             });
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//             res.status(500).json({
-//                 error: err
-//             });
-//         });
-// });
+router.delete('/:userId', UserController.deleteUser);
 
 module.exports = router;
+
